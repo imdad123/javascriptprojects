1e4ed02a0738d0aeaba0b87b03b40191d61bb622
imdad123 <uimdad19@gmail.com>
2024-05-17 21:42:56 +0500

    variables Creation using var let

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 123;
+let accountEmail="test@gmail.com";
+accountPassword="test123";
+let accountState;
+console.table([accountEmail,accountId,accountPassword,accountState]);
+/** Dont use var use let becuase of using scope */
