470e0e7b44ccd31839ccd2f3b70b8387c39ffd31
imdad123 <uimdad19@gmail.com>
2024-05-18 18:55:41 +0500

    datatypes and objectConversion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+let name="imdad";
+let age=30;
+let isLoggedIn=false;
+
+//DataTypes
+//number
+//bigint
+//string ""
+//boolean true/false
+//null its a type
+//undefined its a type
+//symbol unique
+
+//non primative datatype
+//object
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
---
+++ 01_basics/03_objectConversion.js
@@ -0,0 +1,14 @@
+
+//it will be Nan let score="32abc";
+let score=33;
+let scoreInNumber = Number(score);
+console.log(typeof scoreInNumber);
+console.log(scoreInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+// true=>1;false=>0
+//""=>false
+//"imdad"=>true
+let someNumber =33;
+console.log(String(someNumber));
