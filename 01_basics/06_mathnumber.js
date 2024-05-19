c37779d8a697a40e2a4d537d96f12dd24ffbff7a
imdad123 <uimdad19@gmail.com>
2024-05-19 17:10:59 +0500

    Math and number

---
+++ 01_basics/06_mathnumber.js
@@ -0,0 +1,12 @@
+let number=100;
+console.log(number.toFixed());
+let number1 = 100000;
+console.log(number1.toLocaleString());
+
+//MAth
+console.log(Math.floor(4.9695));
+console.log(Math.ceil(5.98));
+console.log(Math.max(4,6,21,5,65,32));
+console.log(Math.min(9,3,0,32,1));
+console.log(Math.abs(-3));
+console.log(Math.floor(Math.random()*100));
