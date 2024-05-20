8f0e6cf997296bed7ee0b9e74924006b999a1aef
Imdad ullah <uimdad19@gmail.com>
2024-05-20 06:37:41 +0000

    functions

---
+++ 01_basics/10_functions.js
@@ -0,0 +1,14 @@
+
+//username here is called parameters
+function login(username)
+{
+    //console.log(!username);
+    if(!username)
+        {
+            console.log("Username not Defined");
+            return;
+        }
+   return username;
+}
+//this is called arguments
+const loggedInMessage = login("Imdad");
